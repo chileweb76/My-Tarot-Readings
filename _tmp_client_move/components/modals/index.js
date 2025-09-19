aeb1f988bdf60782b86826cf34f31f1493f59296
Christopher Hile <christopherhileweb@gmail.com>
2025-09-19 14:01:17 -0400

    Remove now-empty client directory after moving files to repo root

--- _tmp_client_move/components/modals/index.js
+++
@@ -1,4 +0,0 @@
-export { LargeImageWarningModal, ExportSignInModal } from './ExportModals'
-import * as _mods from './ExportModals'
-const modalExports = { LargeImageWarningModal: _mods.LargeImageWarningModal, ExportSignInModal: _mods.ExportSignInModal }
-export default modalExports
